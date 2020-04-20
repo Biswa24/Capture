e9402189f3d462be9d870bc51c6619a8dda0d98a
Biswa24 <sah.biswajeet@gmail.com>
2020-04-20 22:09:23 +0530

    Heroku fixture

--- Main.py
+++ Main.py
@@ -4,14 +4,23 @@ from Screenshot import Screenshot_Clipping
 import os
 
 ob=Screenshot_Clipping.Screenshot()
-DRIVER = ('./chromedriver')
-driver = webdriver.Chrome(DRIVER)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome(excutable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options )
+
+# DRIVER = ('./chromedriver')
+# driver = webdriver.Chrome(DRIVER)
 driver.get('https://itsbiswa.me')
 mg_url=ob.full_Screenshot(driver, save_path=r'.', image_name='Myimage.png')
 driver.quit()
 im1 = Image.open(r'./Myimage.png')
-#im1.show()
-im1 = im1.convert('RGB')
-im1.save(r'./Resume.pdf')
+im1.show()
+# im1 = im1.convert('RGB')
+# im1.save(r'./Resume.pdf')
 
 
