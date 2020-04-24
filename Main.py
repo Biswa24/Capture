77c99b719959495023616a8250327b8f559e4442
Biswa24 <sah.biswajeet@gmail.com>
2020-04-24 17:12:25 +0530

    Delete function added

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import os
-from os import environ
+from os import environ,listdir
 #print(os.path.dirname(os.path.abspath(__file__))+'/chromedriver')
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,12 +18,6 @@ pdf_dir = './static/pdf/'
 def img_gen(url):
 	if url == None :
 		return
-	#driver.get('https://itsbiswa.me')
-	#print(os.path.dirname(os.path.abspath(__file__)))
-
-	# DRIVER = ('./chromedriver/chromedriver_mac')
-	# DRIVER = ('./chromedriver/chromedriver_linux')
-	# driver = webdriver.Chrome(DRIVER)
 
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -40,7 +34,6 @@ def img_gen(url):
 	
 	driver.get(url)
 	img_name =  str(datetime.now().replace(microsecond=0)) +".png"
-	img_name=img_name.replace(" ", "-")
 	mg_url=ob.full_Screenshot(driver, save_path=img_dir, image_name=img_name)
 	driver.quit()
 	return img_name
@@ -57,3 +50,37 @@ def pdf_gen(img_name):
 	return img_name
 
 
+def delete(val,time_diff):
+
+	if val =='img':
+		dir = img_dir
+	elif val == 'pdf':
+		dir = pdf_dir
+	else:
+		print("Error")
+
+	dir_name = listdir(dir)
+	try :
+		dir_name.remove('.DS_Store')
+	except:
+		pass
+	files_name = [i[:-4] for i in dir_name]
+
+	date_time_obj = [datetime.strptime(i, '%Y-%m-%d %H:%M:%S') for i in files_name]
+
+	curr_time = datetime.now().replace(microsecond=0)
+
+	difference = [(curr_time - i).total_seconds() for i in date_time_obj]
+	if len(difference) == 0:
+		print("No files")
+		return
+
+	time = time_diff*60
+
+	for i,j in enumerate(difference):
+		if j > time:
+			img_path = os.path.join(img_dir,dir_name[i])
+			os.remove(img_path)
+			print("Deleted {} file".format(dir_name[i]))
+
+	return 
--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, render_template, url_for
-from Main import img_gen , pdf_gen
+from Main import img_gen , pdf_gen , delete
+from apscheduler.schedulers.background  import BackgroundScheduler
 
 app = Flask(__name__, static_url_path='/static')
 # app.debug = True
+
+time_diff = 5 #minutes
+
+def sensor():
+    delete(val='img',time = time_diff)
+    delete(val='pdf',time = time_diff)
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(sensor,'interval',minutes=time_diff)
+sched.start()
 
 @app.route('/')
 def home():
